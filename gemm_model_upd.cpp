f59beb5612a66922de0e80d3c4f6ebf3c94d9782
egeorgan <evangelos.georganas@intel.com>
2022-06-30 01:01:13 -0700

    Added wt reduction algorithm also for BF16 gemm_upd

--- gemm_model_upd.cpp
+++ gemm_model_upd.cpp
@@ -226,6 +226,11 @@ int gemm_benchmark(int argc, char** argv) {
       LoopSpecs{0, n_threads, 1, true}},
       "A");
 
+  auto wt_vnni_loop = ThreadedLoop<2>({
+      LoopSpecs{0, Mb, 1, true},
+      LoopSpecs{0, Kb, 1, true}},
+      "AB");
+
   auto t1 = getTime();
   
   // benchmark the GEMM
@@ -265,7 +270,7 @@ int gemm_benchmark(int argc, char** argv) {
             gemm_param.c.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), (char*)filter_prv + partial_filter_id * sizeof(DType) * K * M, i_m, i_k, 0, 0, Kb, bk, bm); 
           }
          
-          if ((i_n == 0) && (brcount != Nb)) {
+          if ((i_n == 0) && (brcount != Nb) && (partial_filter_id < 0)) {
             libxsmm_meltw_unary_param zero_param;
             zero_param.out.primary = (void*)gemm_param.c.primary;
             zero_kernel( &zero_param );
@@ -311,6 +316,18 @@ int gemm_benchmark(int argc, char** argv) {
     for (long it = 0; it < n_iters + 1; it++) {
       if (it == 1) t_start = getTime();
 
+      if (n_partial_filters > 0) {
+        init_partial_filters(
+          [&](int* ind) {
+            int partial_filter_id = ind[0], i_m = ind[1], i_k = ind[2];
+            libxsmm_meltw_unary_param zero_param;
+            zero_param.out.primary = (void*)LIBXSMM_ACCESS_RAW(4, sizeof(DType), (char*)filter_prv + partial_filter_id * sizeof(DType) * K * M, i_m, i_k, 0, 0, Kb, bk, bm);
+            zero_kernel( &zero_param );   
+          },
+          [&]() {},
+          [&]() {});
+      }
+
       if (private_trans == 0) {
         inp_trans_loop(
           [&](int* ind) {
@@ -338,12 +355,17 @@ int gemm_benchmark(int argc, char** argv) {
       gemm_loop(
         [&](int* ind) {
           int i_k = ind[0], i_m = ind[1], i_n = ind[2];
+          int partial_filter_id = gemm_loop.get_tid_in_parallel_dim('c', ind);       
           libxsmm_gemm_param gemm_param;
           gemm_param.op.tertiary = (void*)&brcount;
           gemm_param.a.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), tr_output, i_m, i_n, 0, 0, Nb, bn, bm);
           gemm_param.b.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), tr_input, i_k, i_n, 0, 0, Nb, bk, bn);
           gemm_param.c.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), filter, i_m, i_k, 0, 0, Kb, bk, bm);
-          
+
+          if (partial_filter_id >= 0) {
+            gemm_param.c.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), (char*)filter_prv + partial_filter_id * sizeof(DType) * K * M, i_m, i_k, 0, 0, Kb, bk, bm); 
+          }
+                
           if (private_trans > 0) {
             int tid = omp_get_thread_num();
             char is_inp_transposed = inp_trans_tracker[tid*inp_trans_tracker_size+i_k];
@@ -373,7 +395,7 @@ int gemm_benchmark(int argc, char** argv) {
             gemm_param.b.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), (char*)tr_input_prv + tid * sizeof(DType) * N * K, i_k, i_n, 0, 0, Nb, bk, bn);     
           }
           
-          if ((i_n == 0) && (brcount != Nb)) {
+          if ((i_n == 0) && (brcount != Nb) && (partial_filter_id < 0)) {
             libxsmm_meltw_unary_param zero_param;
             zero_param.out.primary = (void*)gemm_param.c.primary;
             zero_kernel( &zero_param );
@@ -381,14 +403,14 @@ int gemm_benchmark(int argc, char** argv) {
 
           brgemm_kernel( &gemm_param );
 
-          if ((i_n + brcount >= Nb) && (brcount != Nb)) {
+          if ((i_n + brcount >= Nb) && (brcount != Nb) && (partial_filter_id < 0)) {
             libxsmm_bfloat16 tmp[bk*bm];
             libxsmm_meltw_unary_param trans_param;
-            trans_param.in.primary  = LIBXSMM_ACCESS_RAW(4, sizeof(DType), filter, i_m, i_k, 0, 0, Kb, bk, bm);
+            trans_param.in.primary  = gemm_param.c.primary;
             trans_param.out.primary = tmp;
             wt_copy_kernel(&trans_param);
             trans_param.in.primary = tmp;
-            trans_param.out.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), filter, i_m, i_k, 0, 0, Kb, bk, bm);
+            trans_param.out.primary = gemm_param.c.primary;
             wt_vnni_kernel(&trans_param);
           }
         },
@@ -401,6 +423,47 @@ int gemm_benchmark(int argc, char** argv) {
           }
         },
         [&]() {if (sizeof(DType) == 2) tilerelease_kernel(NULL);});
+
+      if (n_partial_filters > 0) {
+        wt_reduce_loop(
+          [&](int* ind) {
+            int ltid = ind[0];
+            libxsmm_meltw_unary_param reduce_param;        
+            /* reduce work-related variables  */
+            const int fm_blocking = (bm % 16 == 0) ? 16 : bm;
+            const int reduce_work = Mb * Kb * (bm/fm_blocking) * bk;
+            const int reduce_chunksize = (reduce_work % n_threads == 0) ? (reduce_work / n_threads) : (reduce_work / n_threads) + 1;
+            const int reduce_thr_begin = (ltid * reduce_chunksize < reduce_work) ? (ltid * reduce_chunksize) : reduce_work;
+            const int reduce_thr_end = ((ltid + 1) * reduce_chunksize < reduce_work) ? ((ltid + 1) * reduce_chunksize) : reduce_work;
+            /* Perform reduction here  */
+            reduce_param.in.primary  = (void*)((char*) filter_prv + sizeof(DType) * reduce_thr_begin * fm_blocking);
+            reduce_param.out.primary = (void*)((char*) filter + sizeof(DType) * reduce_thr_begin * fm_blocking);
+            if ((reduce_thr_end - reduce_thr_begin) == reduce_chunksize) {
+              wt_reduce_kernel0( &reduce_param );
+            } else {
+              if ((reduce_thr_end - reduce_thr_begin) > 0) {
+                wt_reduce_kernel1( &reduce_param );
+              }
+            }          
+          },
+          [&]() {},
+          [&]() {});
+
+        wt_vnni_loop(
+          [&](int* ind) {
+            int i_m = ind[0], i_k = ind[1];
+            libxsmm_bfloat16 tmp[bk*bm];
+            libxsmm_meltw_unary_param trans_param;
+            trans_param.in.primary  = LIBXSMM_ACCESS_RAW(4, sizeof(DType), filter, i_m, i_k, 0, 0, Kb, bk, bm);
+            trans_param.out.primary = tmp;
+            wt_copy_kernel(&trans_param);
+            trans_param.in.primary = tmp;
+            trans_param.out.primary = LIBXSMM_ACCESS_RAW(4, sizeof(DType), filter, i_m, i_k, 0, 0, Kb, bk, bm);
+            wt_vnni_kernel(&trans_param);
+          },
+          [&]() {},
+          [&]() {});
+      }
 
       if (it == n_iters) t_end = getTime();
     }
