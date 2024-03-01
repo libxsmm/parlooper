e07401df8d2d09c33afb826bf1453cddcbe36f65
egeorgan <evangelos.georganas@intel.com>
2024-02-29 17:30:56 -0800

    Added detailed timing options

--- samples/gemm/gemm_model_fwd.cpp
+++ samples/gemm/gemm_model_fwd.cpp
@@ -10,6 +10,10 @@
 #include "threaded_loops.h"
 #include "gemm_common_utils.h"
 
+#define N_BRGEMMS_PER_GEMM 128
+#define N_TASKS_PER_GEMM 8
+
+
 template<typename DType>
 int gemm_benchmark(int argc, char** argv) {
   // Setup default GEMM sizes
@@ -189,11 +193,48 @@ int gemm_benchmark(int argc, char** argv) {
   long l1_m_step = m1 * l0_m_step;
   long l1_n_step = n1 * l0_n_step;
 
+  long n_threads = omp_get_max_threads();
+  long n_brgemms = 0;
+
   auto gemm_loop = ThreadedLoop<3>({
       LoopSpecs{0, Kb, k_step, {l1_k_step, l0_k_step}},   // Logical K loop specs
       LoopSpecs{0, Mb, m_step, {l1_m_step, l0_m_step}},   // Logical M loop specs
       LoopSpecs{0, Nb, n_step, {l1_n_step, l0_n_step}}},  // Logical N loop specs
       loop_specs_str);
+
+  for (i = 0; i < n_layers; i++) {
+    gemm_loop( [&](int* ind) {
+      int i_k = ind[0], i_m = ind[1], i_n = ind[2];
+      int m_id = gemm_loop.get_tid_in_parallel_dim('b', ind);
+      int n_id = gemm_loop.get_tid_in_parallel_dim('c', ind);
+      if (m_id == 0 && n_id == 0) {
+        //printf("I am thread with global ID %d and m_id %d and n_is %d\n", gemm_loop.get_tid(ind), m_id, n_id);
+        n_brgemms++;
+      }
+    },
+    [&]() {},
+    [&]() {});
+  }
+
+  printf("In total there are %d BRGEMMS per iter\n", n_brgemms);
+  double *timelines[n_threads];
+  long index_in_timeline[n_threads];
+  long n_record_iters = 5;
+   for (i = 0; i < n_threads; i++) {
+    timelines[i] = (double*) libxsmm_aligned_malloc((n_record_iters*n_brgemms+1)*sizeof(double), 64);
+    memset(timelines[i], 0, (n_record_iters*n_brgemms+1)*sizeof(double));
+  }
+  memset(index_in_timeline, 0, n_threads*sizeof(long));
+
+  long iter_to_record = 300;
+  double profile_times[4*n_threads];
+  long long local_brgemm_ids[n_threads];
+  memset(profile_times, 0, (4*n_threads)*sizeof(double));
+  memset(local_brgemm_ids, 0, n_threads*sizeof(long long));
+  long long n_prof_0 = 0;
+  long long n_prof_1 = 0;
+  long long n_prof_2 = 0;
+  long long n_prof_3 = 0;
 
   // Warmup iteration for i-caches
   for (i = 0; i < n_layers; i++) {
@@ -278,6 +319,13 @@ int gemm_benchmark(int argc, char** argv) {
         [&](int* ind) {
           int i_k = ind[0], i_m = ind[1], i_n = ind[2];
           libxsmm_gemm_param gemm_param;
+          
+          long tid = gemm_loop.get_tid(ind);
+          long long local_brgemm_id = local_brgemm_ids[tid];               
+          double t_before = 0.0, t_after = 0.0;
+          long record_iter = ((it >= iter_to_record) && (it <= iter_to_record+n_record_iters-1)) ? 1 : 0;
+          long cur_index_in_timeline = 0;
+
           gemm_param.op.tertiary = (void*)&brcount;
           gemm_param.a.primary = (void*)((DType*)WGT[i] + i_m * K * bm + i_k * bk * bm );
           if (flat_act == 0) {
@@ -300,7 +348,33 @@ int gemm_benchmark(int argc, char** argv) {
             zero_param.out.primary = (void*)gemm_param.c.primary;
             zero_kernel( &zero_param );
           }
+          t_before = getTime();
           brgemm_kernel( &gemm_param );
+          t_after = getTime();
+          if (record_iter) {
+            cur_index_in_timeline = index_in_timeline[tid];
+            index_in_timeline[tid]++;
+            timelines[tid][cur_index_in_timeline+1] = (t_after-t_before)+timelines[tid][cur_index_in_timeline];
+          }
+          /* Add time based on profile type */
+          if (local_brgemm_id % N_BRGEMMS_PER_GEMM == 0) {
+            /* Streaming from mem/llc both A and B */
+            if (tid == 0) n_prof_0++;
+            profile_times[tid*4 + 0] += (t_after-t_before);
+          } else if (local_brgemm_id % N_BRGEMMS_PER_GEMM < N_TASKS_PER_GEMM) {
+            /* Streaming from mem/llc B, A in L2 */
+            if (tid == 0) n_prof_1++;
+            profile_times[tid*4 + 1] += (t_after-t_before);
+          } else if (local_brgemm_id % N_TASKS_PER_GEMM == 0) {
+            /* Streaming from mem/llc A, B in L2 */
+            if (tid == 0) n_prof_2++;
+            profile_times[tid*4 + 2] += (t_after-t_before);
+          } else {
+            /* Streaming from L2 both A and B*/
+            if (tid == 0) n_prof_3++;
+            profile_times[tid*4 + 3] += (t_after-t_before);
+          }
+          local_brgemm_ids[tid]++;
         },
         [&]() {tileconfig_kernel(NULL);},
         [&]() {tilerelease_kernel(NULL);});
@@ -314,6 +388,59 @@ int gemm_benchmark(int argc, char** argv) {
   printf("Effective model sizes: %.5g GB\n", ((double)sizeof(DType)*(double)n_layers*(double)M*(double)K)/(1024.0*1024.0*1024.0));
   printf("Effective A BW is %.5g GB/s\n", (((double)sizeof(DType)*(double)n_layers*(double)M*(double)K) / (1024.0*1024.0*1024.0))/((t_end-t_start)/(1.0*n_iters)));
   printf("MEASURE %.5g %s_%ld_%ld_%ld_%ld_%ld_%ld_bf%ld_threads%d\n", gflop/((t_end-t_start)/(1.0*n_iters)), loop_specs_str, M, N, K, bm, bn, bk, kbf, omp_get_max_threads());
+
+  long thread_stats_id = 0;
+  printf("\n\nTmeline stats for thread %d\n", thread_stats_id);
+  double n_flops_per_brgemm = 2.0*bm*bn*bk*brcount;
+  double slab_size = 1.0*bm*bk*brcount*sizeof(DType)/1024.0/1024.0/1024.0;
+  double epsilon = 0.0;
+  long layer_to_print = 20;
+
+  FILE *fp = fopen("timeline.txt", "w");
+  for (i = layer_to_print * (n_brgemms/n_layers) * (n_record_iters-1); i < (n_brgemms/n_layers) + layer_to_print * (n_brgemms/n_layers) * (n_record_iters-1) ; i++) {
+    double _t0 = timelines[thread_stats_id][i];
+    double _t1 = timelines[thread_stats_id][i+1];
+    double effective_gflops = 1.0*n_flops_per_brgemm/(1000000000.0*(_t1-_t0));
+    double effective_bw_1_slab = 1.0*slab_size/(1.0*(_t1-_t0));
+    double effective_bw_2_slab = 2.0*slab_size/(1.0*(_t1-_t0));
+    if (i % n_brgemms == 0) {
+      printf("New set of n_brgemms\n");
+    }
+    //printf("Effective Gflops is %.5g\n", effective_gflops);
+    //printf("Effective GB/s 1 slab is %.5g\n", effective_bw_1_slab);
+    //printf("Effective GB/s 2 slab is %.5g\n", effective_bw_2_slab);
+    /* Print two time points: _t0 and _t1 - epsilon with the effective GFLOPS */
+    epsilon = (_t1 -_t0)/1000.0;
+    fprintf(fp, "%.10g\t%.5g\n", _t0, effective_gflops);
+    fprintf(fp, "%.10g\t%.5g\n", _t1-epsilon, effective_gflops);
+    //printf("%.10g\t%.5g\n", _t0, effective_gflops);
+    //rintf("%.10g\t%.5g\n", _t1-epsilon, effective_gflops);
+  }
+  fclose(fp);
+
+  /* Now take averages for the 4 profiles across threads */
+  double prof_avg_0 = 0.0, prof_avg_1 = 0.0, prof_avg_2 = 0.0, prof_avg_3 = 0.0;
+  for (i = 0; i < n_threads; i++) {
+    prof_avg_0 += profile_times[i*4 + 0];
+    prof_avg_1 += profile_times[i*4 + 1];
+    prof_avg_2 += profile_times[i*4 + 2];
+    prof_avg_3 += profile_times[i*4 + 3];
+  }
+  prof_avg_0 = prof_avg_0/(n_threads*1.0*n_prof_0);
+  prof_avg_1 = prof_avg_1/(n_threads*1.0*n_prof_1);
+  prof_avg_2 = prof_avg_2/(n_threads*1.0*n_prof_2);
+  prof_avg_3 = prof_avg_3/(n_threads*1.0*n_prof_3);
+  //printf("Avg 0 / 1 / 2 / 3 are %.5g %.5g %.5g %.5g\n", prof_avg_0, prof_avg_1, prof_avg_2, prof_avg_3);
+  //printf("Count 0 / 1 / 2 / 3 are %lld %lld %lld %lld\n", n_prof_0, n_prof_1, n_prof_2, n_prof_3);
+
+  double core_freq = 1.9;
+  printf("Avg BW (GB/s) for profile 0 (A & B from mem/llc) is %.5g\n", 2.0*slab_size/(1.0*prof_avg_0));
+  printf("Avg BW (Bytes/c) for profile 0 is %.5g\n", (2.0*slab_size/(1.0*prof_avg_0))*1024.0*1024.0*1024.0/(core_freq*1000000000.0));
+  printf("Avg BW (GB/s) for profile 1 (B from mem/llc, A in L2) is %.5g\n", 1.0*slab_size/(1.0*prof_avg_1));
+  printf("Avg BW (Bytes/c) for profile 1 is %.5g\n", (1.0*slab_size/(1.0*prof_avg_1))*1024.0*1024.0*1024.0/(core_freq*1000000000.0));
+  printf("Avg BW (GB/s) for profile 2 (A from mem/llc, B in L2) is %.5g\n", 1.0*slab_size/(1.0*prof_avg_2));
+  printf("Avg BW (Bytes/c) for profile 2 is %.5g\n", (1.0*slab_size/(1.0*prof_avg_2))*1024.0*1024.0*1024.0/(core_freq*1000000000.0));
+  printf("Avg GFLOPS for profile 3 is %.5g\n", 1.0*n_flops_per_brgemm/(1000000000.0*prof_avg_3));
 
   // Free buffers
   libxsmm_free(naive_input);
